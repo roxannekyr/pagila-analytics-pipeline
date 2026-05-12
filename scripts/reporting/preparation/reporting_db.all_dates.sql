8b130b316e5b44faba024e6405fff50ce7451784
Roxani Kyritsi <roxannekyr@users.noreply.github.com>
2026-05-12 18:00:44 +0300

    Add Date Dimension Generator SQL script that is implemented in BigQuery for documentation
    This script generates a Date Dimension table with dates from 2015-01-01 to 2026-12-05 in the reporting database.

---
+++ scripts/reporting/preparation/reporting_db.all_dates.sql
@@ -0,0 +1,23 @@
+/*
+===============================================================================
+Date Dimension Generator (`reporting_db.all_dates`)
+Dialect: BigQuery PostGres SQL
+
+This script dynamically generates a comprehensive Date Dimension table. 
+It creates a continuous, unbroken sequence of dates from 2015-01-01 to 
+2026-12-05 and stages it in the reporting database.
+===============================================================================
+*/
+
+create table if not exists `project-401f4646-3663-4125-aaa`.reporting_db.all_dates(
+  date_column date
+);
+
+insert into `project-401f4646-3663-4125-aaa`.reporting_db.all_dates(date_column)
+select 
+date_add('2015-01-01',interval n day) as date_column
+from
+unnest(generate_array(0,date_diff('2026-12-05','2015-01-01',day))) as n;
+
+select * from `project-401f4646-3663-4125-aaa`.reporting_db.all_dates
+limit 50;
