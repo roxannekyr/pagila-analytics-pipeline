0e1a746ee06dd8d373444a0db5c781007bb5401b
Roxani Kyritsi <roxannekyr@users.noreply.github.com>
2026-05-12 18:40:59 +0300

    Update reporting_db.all_dates.sql

--- scripts/reporting/preparation/reporting_db.all_dates.sql
+++ scripts/reporting/preparation/reporting_db.all_dates.sql
@@ -19,5 +19,6 @@ date_add('2015-01-01',interval n day) as date_column
 from
 unnest(generate_array(0,date_diff('2026-12-05','2015-01-01',day))) as n;
 
-select * from `project-401f4646-3663-4125-aaa`.reporting_db.all_dates
-limit 50;
+--Checking data are inserted correctly in the table
+--select * from `project-401f4646-3663-4125-aaa`.reporting_db.all_dates
+--limit 50;
