4191c20a91bf22594e6571307e079dae1ce27e24
Roxani Kyritsi <roxannekyr@users.noreply.github.com>
2026-05-12 12:16:58 +0300

    Minor adjustments

--- scripts/staging/stg_actor.py
+++ scripts/staging/stg_actor.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Import libraries
-
-# In[1]:
-
-
-# Import libraries
+# Importing libraries
 from google.cloud import bigquery
 import pandas as pd
 from pandas_gbq import to_gbq
@@ -14,18 +6,12 @@ import os
 
 print('Libraries imported successfully')
 
-
-# In[2]:
-
-
-# Set the environment variable for Google Cloud credentials
-# Place the path in which the .json file is located.
+# Setting the environment variable for Google Cloud credentials
+# Placing the path in which the .json file is located.
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\roxan\AppData\Roaming\gcloud\application_default_credentials.json"
-# In[3]:
 
-
-# Set Google Cloud project ID and BigQuery dataset details
+# Setting Google Cloud project ID and BigQuery dataset details
 
 project_id = 'project-401f4646-3663-4125-aaa' # Edit with your project id
 dataset_id = 'staging_db' # Modify the necessary schema name: staging_db, reporting_db etc.
@@ -33,9 +19,7 @@ table_id = 'stg_actor' # Modify the necessary table name: stg_customer, stg_city
 
 # # SQL Query
 
-# In[4]:
-
-# Create the BigQuery client
+# Creating the BigQuery client
 client = bigquery.Client(project=project_id)
 
 query = """
@@ -56,21 +40,18 @@ with base as (
   select * from final
 """
 
-# Execute the query and store the result in a dataframe
+# Executing the query and store the result in a dataframe
 df = client.query(query).to_dataframe()
 
-# Explore some records
+# Exploring some records
 df.head()
 
-# # Write to BigQuery
+# # Writing to BigQuery
 
-# In[5]:
-
-
-# Define the full table ID
+# Defining the full table ID
 full_table_id = f"{project_id}.{dataset_id}.{table_id}"
 
-# Define table schema based on the project description
+# Defining table schema based on the project description
 
 schema = [
     bigquery.SchemaField('actor_id', 'INTEGER'),
@@ -79,13 +60,10 @@ schema = [
     bigquery.SchemaField('actor_last_update', 'DATETIME'),
     ]
 
-# In[6]:
-
-
-# Create a BigQuery client
+# Creating a BigQuery client
 client = bigquery.Client(project=project_id)
 
-# Check if the table exists
+# Checking if the table exists
 def table_exists(client, full_table_id):
     try:
         client.get_table(full_table_id)
@@ -93,39 +71,25 @@ def table_exists(client, full_table_id):
     except Exception:
         return False
 
-# Write the dataframe to the table (overwrite if it exists, create if it doesn't)
+# Writing the dataframe to the table (overwrite if it exists, create if it doesn't)
 if table_exists(client, full_table_id):
-    # If the table exists, overwrite it
+    # If the table exists, overwriting it
     destination_table = f"{dataset_id}.{table_id}"
-    # Write the dataframe to the table (overwrite if it exists)
+    # Writing the dataframe to the table (overwriting if it exists)
     to_gbq(df, destination_table, project_id=project_id, if_exists='replace')
     print(f"Table {full_table_id} exists. Overwritten.")
 else:
-    # If the table does not exist, create it
+    # If the table does not exist, creating it
     job_config = bigquery.LoadJobConfig(schema=schema)
     job = client.load_table_from_dataframe(df, full_table_id, job_config=job_config)
     job.result()  # Wait for the job to complete
     print(f"Table {full_table_id} did not exist. Created and data loaded.")
 
-
-# In[7]:
-
-
 # Converting i.pynb file to .py python executable file.
-
 
 get_ipython().system('python3 -m jupyter nbconvert stg_actor.ipynb --to python')
 
-
-
-# In[ ]:
-
-
 get_ipython().system('python3 -m pip install nbconvert')
-
-
-# In[ ]:
-
 
 get_ipython().system('python3 -m pip install nbconvert -U')
 
