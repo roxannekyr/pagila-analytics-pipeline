84e84180d66af587d5a60688930fa91e6f3ad45b
Roxani Kyritsi <roxannekyr@users.noreply.github.com>
2026-05-12 12:49:26 +0300

    Update stg_language.py

--- scripts/staging/stg_language.py
+++ scripts/staging/stg_language.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Import libraries
-
-# In[ ]:
-
-
-# Import libraries
+# Importing libraries
 from google.cloud import bigquery
 import pandas as pd
 from pandas_gbq import to_gbq
@@ -14,35 +6,24 @@ import os
 
 print('Libraries imported successfully')
 
-
-# In[ ]:
-
-
-# Set the environment variable for Google Cloud credentials
-# Place the path in which the .json file is located.
+# Setting the environment variable for Google Cloud credentials
+# Placing the path in which the .json file is located.
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\roxan\AppData\Roaming\gcloud\application_default_credentials.json"
 
+# Setting the Google Cloud project ID and BigQuery dataset details
 
-# In[ ]:
-
-
-# Set your Google Cloud project ID and BigQuery dataset details
-
-project_id = 'project-401f4646-3663-4125-aaa' # Edit with your project id
-dataset_id = 'staging_db' # Modify the necessary schema name: staging_db, reporting_db etc.
-table_id = 'stg_language' # Modify the necessary table name: stg_customer, stg_city etc.
+project_id = 'project-401f4646-3663-4125-aaa' # project id
+dataset_id = 'staging_db' # schema name: staging_db, reporting_db etc.
+table_id = 'stg_language' # table name: stg_customer, stg_city etc.
 
 
 # # SQL Query
 
-# In[ ]:
-
-
-# Create a BigQuery client
+# Creating a BigQuery client
 client = bigquery.Client(project=project_id)
 
-# Define your SQL query here
+# Defining the SQL query here
 query = """
 with base as (
   select *
@@ -60,22 +41,19 @@ with base as (
   select * from final
 """
 
-# Execute the query and store the result in a dataframe
+# Executing the query and storing the result in a dataframe
 df = client.query(query).to_dataframe()
 
-# Explore some records
+# Exploring some records
 df.head()
 
 
-# # Write to BigQuery
+# # Writting to BigQuery
 
-# In[ ]:
-
-
-# Define the full table ID
+# Defining the full table ID
 full_table_id = f"{project_id}.{dataset_id}.{table_id}"
 
-# Define table schema based on the project description
+# Defining table schema based on the project description
 
 schema = [
     bigquery.SchemaField('language_id', 'INTEGER'),
@@ -83,14 +61,10 @@ schema = [
     bigquery.SchemaField('language_last_update', 'DATETIME')
     ]
 
-
-# In[ ]:
-
-
-# Create a BigQuery client
+# Creating a BigQuery client
 client = bigquery.Client(project=project_id)
 
-# Check if the table exists
+# Checking if the table exists
 def table_exists(client, full_table_id):
     try:
         client.get_table(full_table_id)
@@ -98,37 +72,25 @@ def table_exists(client, full_table_id):
     except Exception:
         return False
 
-# Write the dataframe to the table (overwrite if it exists, create if it doesn't)
+# Writting the dataframe to the table (overwriting if it exists, creating if it doesn't)
 if table_exists(client, full_table_id):
-    # If the table exists, overwrite it
+    # If the table exists, overwriting it
     destination_table = f"{dataset_id}.{table_id}"
-    # Write the dataframe to the table (overwrite if it exists)
+    # Writting the dataframe to the table (overwriting if it exists)
     to_gbq(df, destination_table, project_id=project_id, if_exists='replace')
     print(f"Table {full_table_id} exists. Overwritten.")
 else:
-    # If the table does not exist, create it
+    # If the table does not exist, creating it
     job_config = bigquery.LoadJobConfig(schema=schema)
     job = client.load_table_from_dataframe(df, full_table_id, job_config=job_config)
-    job.result()  # Wait for the job to complete
+    job.result()  # Waitting for the job to complete
     print(f"Table {full_table_id} did not exist. Created and data loaded.")
-
-
-# In[ ]:
-
 
 # Converting i.pynb file to .py python executable file. 
 
 get_ipython().system("python -m jupyter nbconvert stg_language.ipynb --to python --output-dir='../'")
 
-
-# In[ ]:
-
-
 get_ipython().system('python -m pip install nbconvert')
-
-
-# In[ ]:
-
 
 get_ipython().system('python -m pip install nbconvert -U')
 
