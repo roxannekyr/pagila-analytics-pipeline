34b634e83f86260aabdfc5df2a4176401714e86d
Roxani Kyritsi <roxannekyr@users.noreply.github.com>
2026-05-15 20:56:51 +0300

    Refactor SQL query for rental reports

--- scripts/reporting/sql/reporting/rep_rentals_per_customer_and_period.sql
+++ scripts/reporting/sql/reporting/rep_rentals_per_customer_and_period.sql
@@ -1,4 +1,4 @@
-  with cte_rentals as (
+    with cte_rentals as (
 
       select * 
       from `project-401f4646-3663-4125-aaa.staging_db.stg_rental`
@@ -14,55 +14,64 @@
   )
 
   ,  cte_customers as (
-     select *
-     from `project-401f4646-3663-4125-aaa.staging_db.stg_customer`
+
+      select * 
+      from `project-401f4646-3663-4125-aaa.staging_db.stg_customer`
+
   )
 
   ,  cte_rentals_per_period as (
 
       select
           'Day' as reporting_period,
-          date_trunc(cast(rent.rental_rental_date as date), day) as reporting_date,
-          cust.customer_id,
+          date_trunc(date(rentals.rental_rental_date), day) as reporting_date,   -- if timestamp aggregated in daily level
+          cte_customers.customer_id,
           count(*) as total_rentals
-      from cte_rentals as rent left join cte_customers as cust 
-            on rent.rental_customer_id = cust.customer_id
-      group by reporting_period,reporting_date,cust.customer_id
-  
+      from cte_rentals as rentals
+        left join cte_customers
+          on cte_customers.customer_id=rentals.rental_customer_id
+
+      group by reporting_period,reporting_date,cte_customers.customer_id
+
       union all
 
       select
           'Month' as reporting_period,
-          date_trunc(cast(rent.rental_rental_date as date), month) as reporting_date,
-          cust.customer_id,
+          date_trunc(date(rentals.rental_rental_date), month) as reporting_date,   -- if timestamp aggregated in monthly level
+          cte_customers.customer_id,
           count(*) as total_rentals
-      from cte_rentals as rent left join cte_customers as cust 
-            on rent.rental_customer_id = cust.customer_id
-      group by reporting_period,reporting_date,cust.customer_id
+      from cte_rentals as rentals
+        left join cte_customers
+          on cte_customers.customer_id=rentals.rental_customer_id
+
+      group by reporting_period,reporting_date,cte_customers.customer_id
 
       union all
 
       select
           'Year' as reporting_period,
-          date_trunc(cast(rent.rental_rental_date as date), year) as reporting_date,
-          cust.customer_id,
+          date_trunc(date(rentals.rental_rental_date), year) as reporting_date,   -- if timestamp aggregated in yearly level
+          cte_customers.customer_id,
           count(*) as total_rentals
-      from cte_rentals as rent left join cte_customers as cust 
-            on rent.rental_customer_id = cust.customer_id
-      group by reporting_period,reporting_date,cust.customer_id
+      from cte_rentals as rentals
+        left join cte_customers
+          on cte_customers.customer_id=rentals.rental_customer_id
+
+      group by reporting_period,reporting_date,cte_customers.customer_id
 
   )
-  
+
  ,  cte_final as (
 
       select 
           cte_reporting_dates.reporting_period,
           cte_reporting_dates.reporting_date,
-          cte_rentals_per_period.customer_id,
+          cte_customers.customer_id,
           cte_rentals_per_period.total_rentals as total_rentals
-      from cte_reporting_dates inner join cte_rentals_per_period
-        on cte_reporting_dates.reporting_period=cte_rentals_per_period.reporting_period 
-        and cte_reporting_dates.reporting_date=cte_rentals_per_period.reporting_date
+      from cte_reporting_dates left join cte_rentals_per_period
+        on cte_reporting_dates.reporting_period=cte_rentals_per_period.reporting_period and cte_reporting_dates.reporting_date=cte_rentals_per_period.reporting_date
+          left join cte_customers 
+            on cte_rentals_per_period.customer_id=cte_customers.customer_id
       where cte_reporting_dates.reporting_period = 'Day'
 
       union all
@@ -70,11 +79,12 @@
       select 
           cte_reporting_dates.reporting_period,
           cte_reporting_dates.reporting_date,
-          cte_rentals_per_period.customer_id,
+          cte_customers.customer_id,
           cte_rentals_per_period.total_rentals as total_rentals
-      from cte_reporting_dates inner join cte_rentals_per_period
-        on cte_reporting_dates.reporting_period=cte_rentals_per_period.reporting_period 
-        and cte_reporting_dates.reporting_date=cte_rentals_per_period.reporting_date 
+      from cte_reporting_dates left join cte_rentals_per_period
+        on cte_reporting_dates.reporting_period=cte_rentals_per_period.reporting_period and cte_reporting_dates.reporting_date=cte_rentals_per_period.reporting_date
+          left join cte_customers 
+            on cte_rentals_per_period.customer_id=cte_customers.customer_id
       where cte_reporting_dates.reporting_period = 'Month'
 
       union all
@@ -82,13 +92,25 @@
       select 
           cte_reporting_dates.reporting_period,
           cte_reporting_dates.reporting_date,
-          cte_rentals_per_period.customer_id,
+          cte_customers.customer_id,
           cte_rentals_per_period.total_rentals as total_rentals
-      from cte_reporting_dates inner join cte_rentals_per_period 
-        on cte_reporting_dates.reporting_period=cte_rentals_per_period.reporting_period 
-        and cte_reporting_dates.reporting_date=cte_rentals_per_period.reporting_date 
+      from cte_reporting_dates left join cte_rentals_per_period
+        on cte_reporting_dates.reporting_period=cte_rentals_per_period.reporting_period and cte_reporting_dates.reporting_date=cte_rentals_per_period.reporting_date
+          left join cte_customers 
+            on cte_rentals_per_period.customer_id=cte_customers.customer_id
       where cte_reporting_dates.reporting_period = 'Year'
+
  )
+    select * from cte_final
+    order by total_rentals DESC;
 
-  select * from cte_final;
-
+    /* Checking the total numbers for daily level */
+    
+    /*
+    select 
+        sum(total_rentals) as total_rentals
+    from cte_final
+    where reporting_period = 'Day';  
+    */
+    
+   
